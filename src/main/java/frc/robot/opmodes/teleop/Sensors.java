7857a70d3271e821f8e4b4f08a3fae1bc22770b1
CrazyMoosen <73966760+CrazyMoosen@users.noreply.github.com>
2025-10-17 19:48:24 -0400

    prep for workshop 10/18
    updated to 2025.3.2 WPILib
    
    commented out Rev2mDistanceSensor files so that we don't have to waste time downloading the dependencies

--- src/main/java/frc/robot/opmodes/teleop/Sensors.java
+++ src/main/java/frc/robot/opmodes/teleop/Sensors.java
@@ -1,7 +1,7 @@
 package frc.robot.opmodes.teleop;
 
-import com.revrobotics.Rev2mDistanceSensor;
-import com.revrobotics.Rev2mDistanceSensor.Port;
+// import com.revrobotics.Rev2mDistanceSensor;
+// import com.revrobotics.Rev2mDistanceSensor.Port;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
@@ -16,18 +16,18 @@ import frc.robot.opmodes.Opmode;
 
 public class Sensors implements Opmode {
     public DigitalInput beambreak;
-    public Rev2mDistanceSensor dist;
+    // public Rev2mDistanceSensor dist;
 
     public void init(Grinder bot) {
         beambreak = new DigitalInput(5);
-        dist = new Rev2mDistanceSensor(Port.kOnboard);
-        dist.setAutomaticMode(true);
+        // dist = new Rev2mDistanceSensor(Port.kOnboard);
+        // dist.setAutomaticMode(true);
     }
 
     @Override
     public void periodic() {
         SmartDashboard.putBoolean("beambreak", beambreak.get());
-        SmartDashboard.putBoolean("distworks", dist.isRangeValid());
-        SmartDashboard.putNumber("distance", dist.getRange());
+        // SmartDashboard.putBoolean("distworks", dist.isRangeValid());
+        // SmartDashboard.putNumber("distance", dist.getRange());
     }
 }
